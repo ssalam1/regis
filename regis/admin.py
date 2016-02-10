400b4f4d2870a8422bdf4414d72a333b421b2c33
ssalam1 <shadab.salam123@gmail.com>
2016-02-10 14:36:59 +0530

    Site registered with Admin

--- regis/admin.py
+++ regis/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from regis.models import database
 # Register your models here.
 class databaseAdmin(admin.ModelAdmin):
-    list_display=['id','first_name','last_name','email','password']
+    list_display=['pk','first_name','last_name','email','password']
 admin.site.register(database,databaseAdmin)
