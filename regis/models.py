3996061d8a247f4fcaf8b634a001fbf96b2b8eb5
ssalam1 <shadab.salam123@gmail.com>
2016-02-10 12:13:05 +0530

    Form added

--- regis/models.py
+++ regis/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+from django import forms
 # Create your models here.
 class database(models.Model):
     first_name=models.CharField(max_length=255,blank=False,null=False)
     last_name=models.CharField(max_length=255,blank=False,null=False)
     email=models.EmailField(unique=True)
     password=models.CharField(max_length=255)
+
+    
+class register_form(forms.ModelForm):
+    class Meta:
+        model=database
+        fields=['first_name','last_name','email','password']
